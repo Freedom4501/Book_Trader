e603baf2e540d05f552c6f31a1547d99138b7cf2
CSSE Department <csse@maw1.csse.rose-hulman.edu>
2020-04-24 01:15:22 -0400

    connected to mongo

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
 //@ts-check
-
-var rh = rh || {};
-
-var client = require('mongodb').MongoClient;
-
-MongoClient.connect("mongodb://localhost:27017/BookTrader", function(err, db){
-  if(!err){
-    console.log("Connected to Mongo");
-  }
-
-  db.createCollection('user', function(err, collection) {});
+const mongoose = require('mongoose');
+dbURI = "mongodb://localhost:27017/BookTrader";
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+}).then(() => {
+  console.log('Database connection successful');
+  console.log(`Mongoose connected to ${dbURI}`);
+}).catch(err => {
+  console.error(`Database connection error: ${err}`);
 });
+// var rh = rh || {};
+
+// var client = require('mongodb').MongoClient;
+
+// MongoClient.connect("mongodb://localhost:27017/BookTrader", function(err, db){
+//   if(!err){
+//     console.log("Connected to Mongo");
+//   }
+
+//   db.createCollection('user', function(err, collection) {});
+// });
 
 
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+// async function listDatabases(client){
+//   databasesList = await client.db().admin().listDatabases();
+//   console.log("Databases:");
+//   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
 
-class User {
-  constructor(name, uname, pass, address, phone){
-    this.name = name;
-    this.uname = uname;
-    this.pass = pass;
-    this.address = address;
-    this.phone = phone;
-  }
-}
+// class User {
+//   constructor(name, uname, pass, address, phone){
+//     this.name = name;
+//     this.uname = uname;
+//     this.pass = pass;
+//     this.address = address;
+//     this.phone = phone;
+//   }
+// }
 
 
