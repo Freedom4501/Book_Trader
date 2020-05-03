c34e49b64b7def97b98b3b6d6ca228076332af5c
wangj19 <wangj19@rose-hulman.edu>
2020-05-03 15:07:54 -0400

    fix error

--- backendJS/main.js
+++ backendJS/main.js
@@ -133,7 +133,7 @@ router.route('/books')
     }).post((req, res) => {
         // console.log(req);
         var authors = req.body.author;
-        var author_arr = authors.split(", ");
+        var author_arr = authors.split(",");
         var existed = false;
 
         Book.create({    
@@ -152,7 +152,7 @@ router.route('/books')
         });
     });
 
-router.route('/books/:isbn')
+router.route('/books/:isbn/:isbn')
     .get((req, res, next) => {
         Book.findOne({'isbn': req.params.isbn}, function (err, book) {
             if (err) {
