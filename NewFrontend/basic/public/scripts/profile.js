9010219629a85e7906b6ab99707f87d62482e1ca
CSSE Department <csse@wangj19.csse.rose-hulman.edu>
2020-05-08 14:08:55 -0400

    update password

--- NewFrontend/basic/public/scripts/profile.js
+++ NewFrontend/basic/public/scripts/profile.js
@@ -1,7 +1,40 @@
 (function () {
-
+    var couchdb = new PouchDB('http://lim5:000201@137.112.104.118:5984/users');
     function updateProfile(){
         
+    }
+    function updatePassword(){
+        const oldpassword = document.getElementById("inputOldPassword").value;
+        const newpassword = document.getElementById("inputNewPassword").value;
+        const reinputpassword = document.getElementById("reinputNewPassword").value;
+        if(oldpassword==""){
+            console.log("Please enter Old Password");
+            return;
+        }
+        }else if(newpassword == ""){
+            console.log("Please enter New Password");
+            return;
+        }else if(reinputpassword == ""){
+            console.log("Please re-enter Password");
+            return;
+        }else if(newpassword!=reinputpassword){
+            console.log("Unmatched new passwords");
+            return;
+        }
+        const username = `${localStorage.getItem("UsernameLogin")}`
+        couchdb.get(username).then(function (doc) {
+            if(oldpassword!=doc.Password){
+                console.log("Wrong old password");
+                return;
+            }else{
+                doc.Password = newpassword;
+            }
+            return couchdb.put(doc);
+        });then(function () {
+            return couchdb.get(username);
+        }).then(function (doc) {
+            console.log(doc);
+        });
     }
 
 
@@ -20,5 +53,6 @@
     profilePhone.innerHTML = 'Phone:  ' + `${localStorage.getItem("PhoneLogin")}`;
     displaySection.append(profilePhone);
     $("#submitUpdateProfile").on("click", updateProfile);
+    $("#submitUpdatePassword").on("click", updatePassword);
   });
 })();
