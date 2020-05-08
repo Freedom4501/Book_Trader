4a1249150d241e5ad155ccfb9d5b45a3c18adde7
Jiafan Lin <lin@jiafans-mbp.wlan.rose-hulman.edu>
2020-05-08 14:30:34 -0400

    profileupdate

--- NewFrontend/basic/public/scripts/profile.js
+++ NewFrontend/basic/public/scripts/profile.js
@@ -68,9 +68,15 @@ couchdb.put(doc, function(err, response) {
     const profileName = document.createElement("h2");
     profileName.innerHTML = `${localStorage.getItem("NameLogin")}`;
     displaySection.append(profileName);
+    const profileUsername= document.createElement("p");
+    profileUsername.innerHTML = 'Username:  ' + `${localStorage.getItem("UsernameLogin")}`;
+    displaySection.append(profileUsername);
     const profileEmail = document.createElement("p");
     profileEmail.innerHTML = 'Email:  ' + `${localStorage.getItem("EmailLogin")}`;
     displaySection.append(profileEmail);
+    const profilePhone = document.createElement("p");
+    profilePhone.innerHTML = 'Phone:  ' + `${localStorage.getItem("PhoneLogin")}`;
+    displaySection.append(profilePhone);
     $("#submitUpdateProfile").on("click", updateProfile);
     $("#submitUpdatePassword").on("click", updatePassword);
   });
