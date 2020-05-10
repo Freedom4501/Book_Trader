46065197effb4a44dc8e7cfee9c3adc885cab474
Jiafan Lin <lin@jiafans-mbp.wlan.rose-hulman.edu>
2020-05-10 17:27:08 -0400

    update

--- NewFrontend/basic/public/scripts/profile.js
+++ NewFrontend/basic/public/scripts/profile.js
@@ -1,21 +1,20 @@
 (function () {
     var couchdb = new PouchDB('http://lim5:000201@137.112.104.118:5984/users');
     function updateProfile(){
-      const username = `${localStorage.getItem("UsernameLogin")}`
       const newphone = document.getElementById("inputNewPhone").value;
       const newname = document.getElementById("inputNewName").value;
       const newemail = document.getElementById("inputNewEmail").value;
-      const username = `${localStorage.getItem("UsernameLogin")}`
+      //const username = `${localStorage.getItem("UsernameLogin")}`
       couchdb.get(username).then(function (doc) {
       if(newname != ""){
         doc.Name = newname;
-        localStorage.setItem("NameLogin",newname);
+        //localStorage.setItem("NameLogin",newname);
     } if(newemail != ""){
         doc.Email = newemail;
-        localStorage.setItem("EmailLogin",newemail);
+       // localStorage.setItem("EmailLogin",newemail);
    }if(newphone != ""){
     doc.Phone = Number(newphone);
-    localStorage.setItem("PhoneLogin",newphone);
+   // localStorage.setItem("PhoneLogin",newphone);
 }
 
     return couchdb.put(doc);
